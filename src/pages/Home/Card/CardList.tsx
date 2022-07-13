4d71b04ec8f3176a127c1dea6a67d13967d3fc68
syki66 <syeonki66@gmail.com>
2022-07-13 16:21:08 +0900

    feat: change BEM to css Module
    - BEM 방식에서 css Module 방식으로 변경
    - 변수 하나로 카드 사이즈 조절되도록 변경
    - nth-child 랜덤화

--- src/pages/Home/Card/CardList.tsx
+++ src/pages/Home/Card/CardList.tsx
@@ -1,73 +1,72 @@
 import React, { useState } from 'react';
 import Card from './Card';
-import './CardList.scss';
+import styles from './CardList.module.scss';
 import mdrIcon from '../../../assets/Home/Card/mdr.png';
 
+const cardList = [
+  {
+    id: 1,
+    title: 'MDR',
+    desc: 'Biomedical Management',
+    iconPath: mdrIcon,
+    bgColor: '#e6fafe',
+    accentColor: '#05d4f6'
+  },
+  {
+    id: 2,
+    title: 'Clinical Assessment',
+    desc: 'Questionaries and CRF management',
+    iconPath: mdrIcon,
+    bgColor: '#ffecf2',
+    accentColor: '#ff4883'
+  },
+  {
+    id: 3,
+    title: 'Data Transform',
+    desc: 'SDTM and Data query',
+    iconPath: mdrIcon,
+    bgColor: '#fff7eb',
+    accentColor: '#ffb13e'
+  },
+  {
+    id: 4,
+    title: 'Education',
+    desc: 'List of service',
+    iconPath: mdrIcon,
+    bgColor: '#eeecfe',
+    accentColor: '#5643fc'
+  },
+  {
+    id: 5,
+    title: 'Report',
+    desc: 'Analysis and Sharing template',
+    iconPath: mdrIcon,
+    bgColor: '#eeecfe',
+    accentColor: '#5643fc'
+  }
+]
+
 export default function CardList() {
-  const [active, setActive] = useState(false);
+  const [cards, setCards] = useState(cardList);
+  const [isEdit, setIsEdit] = useState(false);
   const onClick = () => {
-    active ? setActive(false) : setActive(true);
+    isEdit ? setIsEdit(false) : setIsEdit(true);
   }
   return (
     <>
       <button onClick={onClick}>홈 화면 편집</button>
-      <div className='container'>
-        <Card
-          title='MDR'
-          desc='Biomedical Management'
-          iconPath={mdrIcon}
-          bgColor='#e6fafe'
-          accentColor='#05d4f6'
-          active={active}
-        />
-        <Card
-          title='Clinical Assessment'
-          desc='Questionaries and CRF management'
-          iconPath={mdrIcon}
-          bgColor='#ffecf2'
-          accentColor='#ff4883'
-          active={active}
-        />
-        <Card
-          title='Data Transform'
-          desc='SDTM and Data query'
-          iconPath={mdrIcon}
-          bgColor='#fff7eb'
-          accentColor='#ffb13e'
-          active={active}
-        />
-        <Card
-          title='Education'
-          desc='List of service'
-          iconPath={mdrIcon}
-          bgColor='#eeecfe'
-          accentColor='#5643fc'
-          active={active}
-        />
-        <Card
-          title='Report'
-          desc='Analysis and Sharing template'
-          iconPath={mdrIcon}
-          bgColor='#eeecfe'
-          accentColor='#5643fc'
-          active={active}
-        />
-        <Card
-          title='MDR'
-          desc='Biomedical Management'
-          iconPath={mdrIcon}
-          bgColor='#eeecfe'
-          accentColor='#5643fc'
-          active={active}
-        />
-        <Card
-          title='MDR'
-          desc='Biomedical Management'
-          iconPath={mdrIcon}
-          bgColor='#eeecfe'
-          accentColor='#5643fc'
-          active={active}
-        />
+      <div className={styles.cards}>
+        {cards.map(card => {
+          return <Card 
+            id={card.id}
+            title={card.title}
+            desc={card.desc}
+            iconPath={card.iconPath}
+            bgColor={card.bgColor}
+            accentColor={card.accentColor}
+            isEdit={isEdit}
+          />
+        })}
       </div>
     </>
   )
